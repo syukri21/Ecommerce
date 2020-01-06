153ca9858abbc94cd5500654c1f8961ca5c43554
Syukri <syukrihsb148@gmail.com>
2020-01-07 02:59:34 +0700

    add scss

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import App from './App.svelte';
+/** @format */
+
+import App from "./App.svelte"
+
+import "material-design-icons/iconfont/material-icons.css"
+import "bootstrap-utilities/bootstrap-utilities.css"
+import "./scss/theme.scss"
+
+import "materialize-css/js/waves.js"
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+    target: document.body
+})
 
-export default app;
+export default app
